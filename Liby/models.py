808da40d22922685267be5ac38845ae57bf132b6
tysion <ctit23.ala@ismt.edu.np>
2025-11-04 07:44:14 +0545

    making the card component

--- Liby/models.py
+++ Liby/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    picture = models.ImageField(upload_to='images/')
+    author = models.CharField(max_length=100, default='Guest')
+    email = models.EmailField(blank=True)
+    description = models.TextField(default='I am a guest of ISMT')
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.name
