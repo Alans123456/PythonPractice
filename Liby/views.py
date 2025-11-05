3247701bb562df584ef4b2ce1cf81400826c8bf7
tysion <ctit23.ala@ismt.edu.np>
2025-11-05 07:49:32 +0545

    connecting the data in library.html

--- Liby/views.py
+++ Liby/views.py
@@ -4,6 +4,6 @@ from .models import Book
 # Create your views here.
 def LibraryHome(request):
     user = request.user
-    books = Book.objects.all()
-    return render(request,'Library.html',{'books':books,'user':user})
+    book = Book.objects.all()
+    return render(request,'Library.html',{'book':book,'user':user})
 
